bc976eae82b39d40a31d5ca83276575ec8326fed
Annemari <ariisi@ttu.ee>
2021-09-29 18:22:28 +0300

    AL03

---
+++ AL03/src/ee/ttu/algoritmid/popularity/Popularity.java
@@ -0,0 +1,45 @@
+package ee.ttu.algoritmid.popularity;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Popularity {
+
+    private int maxCoordinates;
+    private Map<List<Integer>, Integer> coordinates = new HashMap<>();
+    private int numberOfOccurences = 0;
+
+    public Popularity(int maxCoordinates) {
+        this.maxCoordinates = maxCoordinates;
+    }
+
+    /**
+     * @param x, y - coordinates
+     */
+    void addPoint(Integer x, Integer y) {
+        List<Integer> point = List.of(x, y);
+        coordinates.put(point, coordinates.getOrDefault(point, 0) + 1);
+        int numberOfPointOccurances = coordinates.get(point);
+        if (numberOfPointOccurances > numberOfOccurences) {
+            numberOfOccurences = numberOfPointOccurances;
+        }
+    }
+
+    /**
+     * @param x, y - coordinates
+     * @return the number of occurrennces of the point
+     */
+    int pointPopularity(Integer x,Integer y) {
+        return coordinates.get(List.of(x, y));
+    }
+
+
+    /**
+     * @return the number of occurrennces of the most popular point
+     */
+    int maxPopularity() {
+        return numberOfOccurences;
+    }
+
+}
