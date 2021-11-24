153a8b448f7a0d17fe927027db8d61dab37f09d1
Annemari <ariisi@ttu.ee>
2021-11-25 00:25:07 +0200

    AL06.

---
+++ AL06/src/ee/ttu/algoritmid/interestingstamps/InterestingStamps.java
@@ -0,0 +1,34 @@
+package ee.ttu.algoritmid.interestingstamps;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class InterestingStamps {
+
+    public static List<Integer> findStamps(int sum, List<Integer> stampOptions) throws IllegalArgumentException {
+        int[] optimalSolution = new int[sum + 1];
+        int[] lastChosenMark = new int[sum + 1];
+        stampOptions.sort(Collections.reverseOrder());
+        List<Integer> stamps = new ArrayList<>();
+        for (int i = 0; i < sum + 1; i++) {
+            optimalSolution[i] = Integer.MAX_VALUE;
+            for (Integer stamp: stampOptions) {
+                if ((i >= stamp) && (optimalSolution[i] > optimalSolution[i - stamp] + 1)) {
+                   optimalSolution[i] = optimalSolution[i - stamp] + 1;
+                   lastChosenMark[i] = stamp;
+                }
+            }
+        }
+        int n = sum;
+        while (n > 0) {
+            stamps.add(lastChosenMark[n]);
+            n -= lastChosenMark[n];
+        }
+        return stamps;
+    }
+
+    private static boolean isInterestingStamp(Integer stamp) {
+        return stamp != 1 && stamp % 10 != 0;
+    }
+}
