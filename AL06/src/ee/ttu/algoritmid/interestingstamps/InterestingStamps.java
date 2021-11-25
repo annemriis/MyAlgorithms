7b0119c56e79613bd5084db3b28ce11c73232849
Annemari <ariisi@ttu.ee>
2021-11-25 20:30:58 +0200

    AL06.

--- AL06/src/ee/ttu/algoritmid/interestingstamps/InterestingStamps.java
+++ AL06/src/ee/ttu/algoritmid/interestingstamps/InterestingStamps.java
@@ -3,6 +3,7 @@ package ee.ttu.algoritmid.interestingstamps;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InterestingStamps {
 
@@ -12,15 +13,24 @@ public class InterestingStamps {
             }
         int[] optimalSolution = new int[sum + 1];
         int[] lastChosenMark = new int[sum + 1];
+        int[] optimalInteresting = new int[sum + 1];
         stampOptions.sort(Collections.reverseOrder());
+        stampOptions = stampOptions.stream().filter(stamp -> stamp <= sum).collect(Collectors.toList());
         List<Integer> stamps = new ArrayList<>();
         int lastIndex = stampOptions.size() - 1;
         for (int i = stampOptions.get(lastIndex); i < sum + 1; i++) {
             optimalSolution[i] = Integer.MAX_VALUE;
+            optimalInteresting[i] = 0;
             for (Integer stamp: stampOptions) {
-                if ((i >= stamp) && (optimalSolution[i] > optimalSolution[i - stamp] + 1)) {
-                   optimalSolution[i] = optimalSolution[i - stamp] + 1;
-                   lastChosenMark[i] = stamp;
+                if ((i >= stamp) && (optimalSolution[i] >= optimalSolution[i - stamp] + 1)) {
+                    if (optimalSolution[i] > optimalSolution[i - stamp] + 1) {
+                        optimalSolution[i] = optimalSolution[i - stamp] + 1;
+                        lastChosenMark[i] = stamp;
+                    } else if (isInterestingStamp(stamp) && optimalInteresting[i] < optimalInteresting[i - stamp] + 1) {
+                        optimalInteresting[i] = optimalInteresting[i - stamp] + 1;
+                        optimalSolution[i] = optimalSolution[i - stamp] + 1;
+                        lastChosenMark[i] = stamp;
+                    }
                 }
             }
         }
