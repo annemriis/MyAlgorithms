755520824f2d0856ca0b97b935d352d83dc2721c
Annemari <ariisi@ttu.ee>
2021-11-25 23:08:25 +0200

    AL06.

--- AL06/src/ee/ttu/algoritmid/interestingstamps/InterestingStamps.java
+++ AL06/src/ee/ttu/algoritmid/interestingstamps/InterestingStamps.java
@@ -62,14 +62,4 @@ public class InterestingStamps {
         return stamp != 1 && stamp % 10 != 0;
     }
 
-    public static void main(String[] args) {
-        ArrayList<Integer> stamps = new ArrayList<>();
-        stamps.add(1);
-        stamps.add(10);
-        stamps.add(24);
-        stamps.add(30);
-        stamps.add(33);
-        stamps.add(36);
-        System.out.println(findStamps(100, stamps));
-    }
 }
